b2185ada77b67f68605879c3b10c0e296072c6b2
Hoang van Viet Anh <viet_anh@lampart-vn.com>
2021-10-25 07:02:43 +0700

    create header footer

---
+++ wp-content/themes/kennguyen/footer.php
@@ -0,0 +1,100 @@
+<!-- Modal-->
+<div class="modal fade" id="modal_logout" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-body text-center">
+                <button class="btn-close" type="button" data-bs-dismiss="modal" aria-label="Close"></button>
+                <div class="pt-30">Do you want to log out of your account?</div>
+            </div>
+            <div class="modal-footer">
+                <button class="btn btn_cancel" type="button" data-bs-dismiss="modal">Cancel</button>
+                <button class="btn btn_more" type="button">Log out</button>
+            </div>
+        </div>
+    </div>
+</div>
+<footer class="footer pt-40 pb-30">
+    <div class="wraper">
+        <div class="row">
+            <div class="col-12 col-lg-5">
+                <div class="content_main flexBox">
+                    <div class="images"> <img class="imgAuto" src="<?php bloginfo('template_directory') ?>/common/images/avatar.png" alt=""/></div>
+                    <div class="content">
+                        <p>Lia Griffith is a designer, maker, artist, and author. Since launching her handcrafted lifestyle site with her first paper rose in 2013, Lia and her team have developed thousands of original DIY templates, SVG cut files, and tutorials to empower others who want to learn, make, and create. While paper flowers are where this journey began, Lia is most passionate about helping others find joy in crafting and reopen the door to their creative soul. She believes in changing lives one craft at a time. Join us.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-lg-7">
+                <div class="row">
+                    <div class="col-12 col-md-7">
+                        <div class="row">
+                            <div class="col-12 col-md-6 toggle_parent">
+                                <h4 class="ttl toggle_btn">ABOUT</h4>
+                                <div class="toggle_content">
+                                    <ul class="list_link">
+                                        <li>OUR TEAM</li>
+                                        <li>CONTACT US</li>
+                                        <li>LEGAL</li>
+                                        <li>FAQ</li>
+                                        <li>PRESS FEATURES</li>
+                                        <li>PARTNERS &amp; AFILIATES</li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="col-12 col-md-6 toggle_parent">
+                                <h4 class="ttl toggle_btn">RESOURCES</h4>
+                                <div class="toggle_content">
+                                    <ul class="list_link">
+                                        <li>PAPER FLOWER</li>
+                                        <li>GLOSSARY</li>
+                                        <li>FELT FLOWER GLOSSARY</li>
+                                        <li>VIDEO LIBRARY</li>
+                                        <li>SHOP</li>
+                                        <li>GIVE A GIFT</li>
+                                        <li>BECOME A MEMBER</li>
+                                        <li>MATERIAL SOURCES</li>
+                                        <li>FREEBIES</li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-5">
+                        <h4 class="ttl">STAY CONNECTED</h4>
+                        <ul class="social flexBox midle mt-20">
+                            <li> <a href="#"> <img src="<?php bloginfo('template_directory') ?>/common/images/social_01.svg" alt=""/></a></li>
+                            <li> <a href="#"> <img src="<?php bloginfo('template_directory') ?>/common/images/social_02.svg" alt=""/></a></li>
+                            <li> <a href="#"> <img src="<?php bloginfo('template_directory') ?>/common/images/social_03.svg" alt=""/></a></li>
+                            <li> <a href="#"> <img src="<?php bloginfo('template_directory') ?>/common/images/social_04.svg" alt=""/></a></li>
+                            <li> <a href="#"> <img src="<?php bloginfo('template_directory') ?>/common/images/social_05.svg" alt=""/></a></li>
+                        </ul>
+                        <div class="text mt-20">
+                            <p>Join our email list to learn about new projects, discounts, and membership perks!</p>
+                        </div>
+                        <div class="form_submit pt-20">
+                            <form action="">
+                                <input class="input" type="text" placeholder="Your Email Adress"/>
+                                <input class="submit" type="submit" value="JOIN NOW"/>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+<div id="fb-root"></div>
+<script async="" defer="" crossorigin="anonymous" src="https://connect.facebook.net/vi_VN/sdk.js#xfbml=1&amp;version=v12.0" nonce="EsXrjZVa"></script>
+<script src="<?php bloginfo('template_directory') ?>/common/js/jquery.min.js"></script>
+<script src="<?php bloginfo('template_directory') ?>/common/js/select_custom.js"></script>
+<script src="<?php bloginfo('template_directory') ?>/common/js/jquery.matchHeight-min.js"></script>
+<script src="<?php bloginfo('template_directory') ?>/common/js/owl.carousel.js"></script>
+<script src="<?php bloginfo('template_directory') ?>/common/js/slick.js"></script>
+<script src="<?php bloginfo('template_directory') ?>/common/js/popper.min.js"></script>
+<script src="<?php bloginfo('template_directory') ?>/common/js/bootstrap.min.js"></script>
+<script src="<?php bloginfo('template_directory') ?>/common/js/main.js"></script>
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
---
+++ wp-content/themes/kennguyen/header.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no"/>
+    <title>Ken Nguyen</title>
+    <meta name="description" content="test"/>
+    <meta name="keywords" content="test"/>
+    <meta property="og:title" content="test"/>
+    <meta property="og:image" content="<?php bloginfo('template_directory') ?>/common/images/banner_01.jpg"/>
+    <meta property="og:site_name" content="test"/>
+    <link rel="icon" href="<?php bloginfo('template_directory') ?>/common/images/favicon.ico"/>
+    <link rel="stylesheet" href="<?php bloginfo('template_directory') ?>/common/css/bootstrap.min.css"/>
+    <link rel="stylesheet" href="<?php bloginfo('template_directory') ?>/common/css/owl.carousel.min.css"/>
+    <link rel="stylesheet" href="<?php bloginfo('template_directory') ?>/common/css/common.css"/>
+    <link rel="stylesheet" href="<?php bloginfo('template_directory') ?>/common/css/styles.css"/>
+    <?php wp_head(); ?>
+</head>
+<body nav_active="nav_active" <?php body_class(); ?>>
+<?php wp_body_open(); ?>
+<header class="header active_search" id="header">
+    <div class="header-top">
+        <div class="wraper">
+            <button class="navbar-toggler toggle_class" type="button" data-bs-toggle="modal" data-bs-target="#navbarSupportedContent"><span class="navbar-toggler-icon"></span></button>
+        </div>
+    </div>
+    <div class="header-bottom">
+        <div class="wraper flexBox space midle bottom">
+            <a class="logo" href="index.html">
+                <img class="imgAuto" src="<?php bloginfo('template_directory') ?>/common/images/logo.svg" alt=""/>
+            </a>
+            <div class="header-right flexBox midle end">
+                <nav class="navbar navbar-expand-lg modal fade collapse" id="navbarSupportedContent" navbar-collapsetabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog">
+                        <div class="header_nav">
+                            <div class="nav flexBox midle space">
+                                <ul class="flexBox midle">
+                                    <li class="nav-item"><a class="nav-link" href="category.html">CRAFT COLECTION</a></li>
+                                    <li class="nav-item"><a class="nav-link" href="contact.html">CRAFT ACADEMY</a></li>
+                                </ul>
+                                <ul class="flexBox midle">
+                                    <li class="nav-item"><a class="nav-link" href="#" target="_blank">SHOP</a></li>
+                                    <li class="nav-item"><a class="nav-link" href="upgrade.html">UPGRADE TO DAY</a></li>
+                                    <li class="nav-item"><a class="nav-link" href="news.html">MY CRAFT ROOM</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+            <div class="nav_setting dropdown">
+                <div class="btn_setting dropdown-toggle" id="dropdownMenu2" data-bs-toggle="dropdown" aria-expanded="false"><img src="<?php bloginfo('template_directory') ?>/common/images/icon/icon_setting.svg" alt=""/></div>
+                <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
+                    <ul>
+                        <li><a class="dropdown-item" href="login.html"><i class="icon"> <img src="<?php bloginfo('template_directory') ?>/common/images/icon/icon_login.png" alt=""/></i>Login</a></li>
+                        <li><a class="dropdown-item" href="singup.html"><i class="icon"> <img src="<?php bloginfo('template_directory') ?>/common/images/icon/icon_signup.png" alt=""/></i>Sign up</a></li>
+                        <li><a class="dropdown-item" href="manager.html"><i class="icon"> <img src="<?php bloginfo('template_directory') ?>/common/images/icon/icon_profile.png" alt=""/></i>Profile manager</a></li>
+                        <li><a class="dropdown-item" href="logout.html" data-bs-toggle="modal" data-bs-target="#modal_logout"> <i class="icon"> <img src="<?php bloginfo('template_directory') ?>/common/images/icon/icon_logout.png" alt=""/></i>Log out</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+
+<div id="content" class="site-content">
