f4792d1d6c4edde1790064adaa4f2aaf0c82caf6
viet_anh <viet_anh@lampart-vn.com>
2021-11-12 06:52:19 +0700

    Fix bug email click

--- wp-content/themes/kennguyen/footer.php
+++ wp-content/themes/kennguyen/footer.php
@@ -360,7 +360,7 @@
         return re.test(email);
     }
 
-    $("form input[type=submit]").click(function(e) {
+    $("form input[name=submit-form-guest-email]").click(function(e) {
         e.preventDefault();
 
         let email = $("input[name=guest_email]").val()
--- wp-content/themes/kennguyen/template-parts/order/form-email.php
+++ wp-content/themes/kennguyen/template-parts/order/form-email.php
@@ -3,7 +3,7 @@ if (check_membership() != 1) {
     echo '<div class="form_submit pt-20 text-center">
                                         <form method="post" class="submit-form-email">
                                             <input class="input" name="guest_email" type="text" placeholder="Your Email Adress"/>
-                                            <input class="submit" type="submit" value="JOIN NOW" id="submit-guest-email"/>
+                                            <input class="submit" type="submit" name="submit-form-guest-email" value="JOIN NOW" id="submit-guest-email"/>
                                         </form>
                                         <div class="spinner-border submit-loading-email mt-10 ml-4" role="status" style="display: none"></div>
                                     </div>';
