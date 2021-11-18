1e2553098f097c17bc13114f590bc9c2ce9dce43
Hoang Ngoc Thanh Nam <hntnam98@gmail.com>
2021-11-19 01:10:36 +0700

    hot fix

--- wp-content/themes/kennguyen/core/migrate-customer.php
+++ wp-content/themes/kennguyen/core/migrate-customer.php
@@ -25,7 +25,7 @@ function my_plugin_create_db() {
     if($wpdb->get_var("SHOW TABLES LIKE '$table_customer'" ) != $table_customer){
         $sql= "CREATE TABLE $table_customer (
             id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-            email VARCHAR(255) NOT NULL UNIQUE,
+            email VARCHAR(255) NOT NULL,
             password VARCHAR(255) NOT NULL,
             first_name VARCHAR(255),
             last_name VARCHAR(255),
