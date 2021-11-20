371921476ab22fbcee49e54a9655b28784ee8c9e
Hoang Ngoc Thanh Nam <hntnam98@gmail.com>
2021-11-20 16:24:45 +0700

    hot fix manager

--- wp-content/themes/kennguyen/manager.php
+++ wp-content/themes/kennguyen/manager.php
@@ -142,12 +142,11 @@ get_header();
                                             </div>
                                         </a>
                                     </div>
-                            <?php    } } else {?>
-                            <div class="column col-6 col-md-4 pt-5">
-                                <p>You don't download anything yet!</p>
-                            </div>
-                                <?php}?>
-                            <?php
+                            <?php    } } else { ?>
+                                <div class="column col-6 col-md-4 pt-5">
+                                    <p>You don't download anything yet!</p>
+                                </div>
+                                <?php }
                                 if ($limit == count($arrayPost)) { ?>
                                     <div class="mt-40 text-center">
                                         <form method="post" action="">
