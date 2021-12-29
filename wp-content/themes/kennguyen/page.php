3a2b5536cb98ebfe853cab0139876b35542735fb
viet_anh <viet_anh@lampart-vn.com>
2021-12-29 08:39:44 +0700

    Feedback

--- wp-content/themes/kennguyen/page.php
+++ wp-content/themes/kennguyen/page.php
@@ -12,27 +12,29 @@
 get_header();
 ?>
 
-    <div id="primary" class="content-area">
         <main id="main" class="site-main">
+            <section class="course_detail pt-40 pb-40">
+                <div class="wraper">
+                    <?php
 
-            <?php
+                    // Start the Loop.
+                    while ( have_posts() ) :
+                        the_post();
 
-            // Start the Loop.
-            while ( have_posts() ) :
-                the_post();
 
-                get_template_part( 'template-parts/content/content', 'page' );
+                        the_content();
+//                get_template_part( 'template-parts/content/content', 'page' );
 
-                // If comments are open or we have at least one comment, load up the comment template.
-                if ( comments_open() || get_comments_number() ) {
-                    comments_template();
-                }
+                        // If comments are open or we have at least one comment, load up the comment template.
+                        if ( comments_open() || get_comments_number() ) {
+                            comments_template();
+                        }
 
-            endwhile; // End the loop.
-            ?>
-
+                    endwhile; // End the loop.
+                    ?>
+                </div>
+            </section>
         </main><!-- #main -->
-    </div><!-- #primary -->
 
 <?php
 get_footer();
