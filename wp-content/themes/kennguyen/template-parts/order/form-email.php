1a99a0ec1615d44cf1545c988ef462cce19cdd54
viet_anh <viet_anh@lampart-vn.com>
2022-01-19 15:46:13 +0700

    Fix bug mail

--- wp-content/themes/kennguyen/template-parts/order/form-email.php
+++ wp-content/themes/kennguyen/template-parts/order/form-email.php
@@ -1,11 +1,9 @@
 <?php
-if (check_membership() < 1) {
-    echo '<div class="form_submit pt-20 text-center">
+echo '<div class="form_submit pt-20 text-center">
                                         <form method="post" class="submit-form-email">
                                             <input class="input" name="guest_email" type="text" placeholder="Your Email Adress"/>
                                             <input class="submit" type="submit" name="submit-form-guest-email" value="JOIN NOW" id="submit-guest-email"/>
                                         </form>
                                         <div class="spinner-border submit-loading-email mt-10 ml-4" role="status" style="display: none"></div>
                                     </div>';
-}
 ?>
