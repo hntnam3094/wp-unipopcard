cffe6d5ed9527e364758d73e20ede225f281110a
Thanh Nam <hntnam98@gmail.com>
2022-10-14 11:02:42 +0700

    Add plugins

---
+++ wp-content/plugins/klavioy/klavioy.php
@@ -0,0 +1,51 @@
+<?php /*
+   Plugin Name: Klavioy custom for Ken
+   Description: Klavioy custom for Ken
+   Author: Thanh Nam
+   Version: 1.0
+   */
+
+
+
+add_action('add_subscription', 'function_add_subscription', 10, 3);
+
+function function_add_subscription($first_name, $last_name, $email) {
+
+    global $va_options;
+    $listId = $va_options['klavioy_list_id'];
+    $apiKey = $va_options['klavioy_api_key'];
+    if (empty($listId) || empty($apiKey)) {
+        return true;
+    }
+    $curl = curl_init();
+    curl_setopt_array($curl, array(
+        CURLOPT_URL => 'https://a.klaviyo.com/api/v2/list/'.$listId.'/members?api_key='.$apiKey,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'POST',
+        CURLOPT_POSTFIELDS =>'
+        {
+             "profiles": [
+                  {
+                       "email": "'.$email.'",
+                       "first_name": "'.$first_name.'",
+                       "last_name": "'.$last_name.'"
+                  }
+             ]
+        }
+        ',
+        CURLOPT_HTTPHEADER => array(
+            'accept: application/json',
+            'content-type: application/json'
+        ),
+    ));
+
+    $response = curl_exec($curl);
+
+    curl_close($curl);
+    return $response;
+}
