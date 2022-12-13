16e9765def7d84f8f003c916b6a48f4a94cd7867
ngoc_luan <ngocluantdt@gmail.com>
2022-12-13 20:25:38 +0700

    change release.sh

--- release.sh
+++ release.sh
@@ -2,7 +2,16 @@ echo "#####Git pull#####"
 
 git pull
 
+
 echo "#####Chmod folders and files#####"
 
 find . -type d -exec chmod 0755 {} \;
 find . -type f -exec chmod 0644 {} \;
+
+echo "#####Chown folders and files#####"
+chown -R devun3311:devun3311 *
+cd .git
+chown -R devun3311:devun3311 *
+
+
+echo "#####Done#####"
